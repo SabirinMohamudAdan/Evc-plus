5334762c9bcf12abd9a95e1e647e0e1a7b966ec3
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-02-12 14:02:36 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -153,7 +153,7 @@ function App() {
               <option value="9">Bill Payment</option>
             </select>
           </div>
-          <button type="submit" className="w-full bg-green-700text-white p-2 rounded hover:bg-teal-700">submit</button>
+          <button type="submit" className="w-full bg-green-700 ~text-white p-2 rounded hover:bg-teal-700">submit</button>
         </form>
       )}
 
